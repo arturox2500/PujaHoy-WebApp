30b28905fe9b5b26b2e1b751d0402a0e9a95db6e
Iván Gutiérrez González <146970754+IvanGutierrrez@users.noreply.github.com>
2025-02-17 11:39:57 +0100

    Add files via upload

---
+++ pujahoy/src/main/java/com/webapp08/pujahoy/PujahoyApplication.java
@@ -0,0 +1,13 @@
+package com.webapp08.pujahoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PujahoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PujahoyApplication.class, args);
+	}
+
+}
---
+++ pujahoy/src/test/java/com/webapp08/pujahoy/PujahoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp08.pujahoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PujahoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
