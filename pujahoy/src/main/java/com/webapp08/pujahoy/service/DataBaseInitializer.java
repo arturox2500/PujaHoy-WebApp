d4a647fd61e1aeb29d423d7feaeaed49efff554f
Iván Gutiérrez González <146970754+IvanGutierrrez@users.noreply.github.com>
2025-02-27 17:52:05 +0100

    Update DataBaseInitializer.java

--- pujahoy/src/main/java/com/webapp08/pujahoy/service/DataBaseInitializer.java
+++ pujahoy/src/main/java/com/webapp08/pujahoy/service/DataBaseInitializer.java
@@ -2,16 +2,15 @@ package com.webapp08.pujahoy.service;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.webapp08.pujahoy.model.Usuario;
+import com.webapp08.pujahoy.model.Producto;
 import com.webapp08.pujahoy.repository.UsuarioRepository;
+import com.webapp08.pujahoy.repository.ProductoRepository;
 
 import jakarta.annotation.PostConstruct;
 
@@ -20,7 +19,10 @@ import jakarta.annotation.PostConstruct;
 public class DataBaseInitializer {
 
     @Autowired
-	private UsuarioRepository UserRepository;
+	private UsuarioRepository userRepository;
+
+	@Autowired
+	private ProductoRepository productoRepository;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
@@ -29,9 +31,14 @@ public class DataBaseInitializer {
 	public void init() throws IOException, URISyntaxException {
 			Usuario user1 = new Usuario("Juan", 5, "Juanito", "juanElGrande@gmail.com", "descripción prueba arturo", true, passwordEncoder.encode("pass"), "ADMIN");
 			Usuario user2 = new Usuario("Pedro", 2, "Pedrito", "pedrosimple@gmail.com", "descripcion", true, passwordEncoder.encode("pass"), "USER");
+			Usuario user3 = new Usuario("Pablo", 3, "Pablito", "pablo@gmail.com", "descripcion", true, passwordEncoder.encode("pass"), "USER");
 
-			UserRepository.save(user1);
-			UserRepository.save(user2);
+			userRepository.save(user1);
+			userRepository.save(user2);
+			userRepository.save(user3);
+	
+			Producto product1 = new Producto("Producto1","En venta", user2);
 
+			productoRepository.save(product1);
 	}
 }
