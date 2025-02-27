cfea47faed99373531d872292eabd9423f6faa52
Iván Gutiérrez González <146970754+IvanGutierrrez@users.noreply.github.com>
2025-02-27 17:53:28 +0100

    Update ProductoRepository.java

--- pujahoy/src/main/java/com/webapp08/pujahoy/repository/ProductoRepository.java
+++ pujahoy/src/main/java/com/webapp08/pujahoy/repository/ProductoRepository.java
@@ -1,5 +1,7 @@
 package com.webapp08.pujahoy.repository;
 
+import java.util.Optional;
+
 //import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,5 @@ import com.webapp08.pujahoy.model.Producto;
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
     
+    Optional<Producto> findByDatos(String id);
 }
