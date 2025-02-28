fe3da4a384356c46f69e0f3187c85a660cb53be5
jae9104 <j.andres.2022@alumnos.urjc.es>
2025-02-28 11:25:10 +0100

    Añadido /logout e interceptor para pasar el authentication en todos los header

---
+++ pujahoy/src/main/java/com/webapp08/pujahoy/config/AuthenticationInterceptor.java
@@ -0,0 +1,23 @@
+package com.webapp08.pujahoy.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthenticationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (modelAndView != null) {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            boolean isAuthenticated = authentication != null && authentication.isAuthenticated() && !(authentication.getPrincipal() instanceof String && authentication.getPrincipal().equals("anonymousUser"));
+            modelAndView.addObject("authenticated", isAuthenticated);
+        }
+    }
+}
--- pujahoy/src/main/java/com/webapp08/pujahoy/controller/HomeController.java
+++ pujahoy/src/main/java/com/webapp08/pujahoy/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.webapp08.pujahoy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.ui.Model;
+
 
 
 @Controller
