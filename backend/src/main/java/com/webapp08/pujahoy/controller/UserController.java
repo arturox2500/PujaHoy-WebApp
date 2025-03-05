8949f69c6f510a7e0d349d4e420205a1d9021e93
arturox2500 <gutierrezarturox@gmail.com>
2025-03-05 16:13:57 +0100

    rating product fixed

--- backend/src/main/java/com/webapp08/pujahoy/controller/UserController.java
+++ backend/src/main/java/com/webapp08/pujahoy/controller/UserController.java
@@ -428,8 +428,10 @@ public class UserController {
             }
             Optional<UserModel> user = userService.findById(trans.get().getBuyer().getId());
             Optional<UserModel> user1 = userService.findByName(principal.getName());
+            System.out.println("ANOOOO");
+            System.out.println(user1.get().getId().equals(user.get().getId()));
             if (user.isPresent() && user1.isPresent()) {
-                if (user.get().determineUserType().equals("Usuario Registrado")
+                if (user.get().determineUserType().equals("Registered User")
                         && user1.get().getId().equals(user.get().getId())) {
                     model.addAttribute("id", id);
                     return "ratingProduct";
