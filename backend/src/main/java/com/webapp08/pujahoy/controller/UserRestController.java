77d4089925cda2463efc42842af6a728bf0a782b
IvanGutierrrez <i.gutierrez.2022@alumnos.urjc.es>
2025-03-17 15:03:22 +0100

    Add Get user by id

--- backend/src/main/java/com/webapp08/pujahoy/controller/UserRestController.java
+++ backend/src/main/java/com/webapp08/pujahoy/controller/UserRestController.java
@@ -1,11 +1,13 @@
 package com.webapp08.pujahoy.controller;
 
+import com.webapp08.pujahoy.dto.PublicUserDTO;
 import com.webapp08.pujahoy.model.UserModel;
 import com.webapp08.pujahoy.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 
 @RestController
@@ -16,10 +18,17 @@ public class UserRestController {
     private UserService userService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserModel> getUserById(@PathVariable Long id) {
-        return userService.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public PublicUserDTO getUserById(@PathVariable Long id) { //Get user by id
+        return userService.findUser(id);
     }
 
+    
+    //@GetMapping("/{id}/image")//Get user image
+    
+
+    //@PutMapping("/{id}")//Update user
+
+    
+    //@PostMapping("/{id}") //Rate user
+    
 }
--- backend/src/main/java/com/webapp08/pujahoy/service/UserService.java
+++ backend/src/main/java/com/webapp08/pujahoy/service/UserService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.webapp08.pujahoy.dto.PublicUserDTO;
+import com.webapp08.pujahoy.dto.UserMapper;
 import com.webapp08.pujahoy.model.Product;
 import com.webapp08.pujahoy.model.UserModel;
 import com.webapp08.pujahoy.repository.UserModelRepository;
@@ -14,7 +16,14 @@ public class UserService {
 
     @Autowired
 	private UserModelRepository repository;
+
+	@Autowired
+	private UserMapper mapper;
     
+	public PublicUserDTO findUser(Long id) {
+		return mapper.toDTO(repository.findById(id).get());
+	}
+
     public Optional<UserModel> findById(Long id) {
 		return repository.findById(id);
 	}
