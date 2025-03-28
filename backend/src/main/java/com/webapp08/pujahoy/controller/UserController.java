f3d9fdff429bc490ae2cf7e951a4bab4af9141b0
jae9104 <j.andres.2022@alumnos.urjc.es>
2025-03-28 11:16:42 +0100

    minor fixes

--- backend/src/main/java/com/webapp08/pujahoy/controller/UserController.java
+++ backend/src/main/java/com/webapp08/pujahoy/controller/UserController.java
@@ -2,8 +2,6 @@ package com.webapp08.pujahoy.controller;
 
 import java.util.Optional;
 
-import javax.sql.rowset.serial.SerialBlob;
-
 import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -35,7 +32,6 @@ import com.webapp08.pujahoy.service.UserService;
 import com.webapp08.pujahoy.dto.ProductDTO;
 import com.webapp08.pujahoy.dto.PublicUserDTO;
 import com.webapp08.pujahoy.dto.RatingDTO;
-import com.webapp08.pujahoy.dto.UserDTO;
 import com.webapp08.pujahoy.dto.UserEditDTO;
 import com.webapp08.pujahoy.model.Product;
 import com.webapp08.pujahoy.service.ProductService;
