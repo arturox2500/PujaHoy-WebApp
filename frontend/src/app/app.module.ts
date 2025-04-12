7a80589127c16c1e08cf04da316deeb9810cdff9
arturox2500 <gutierrezarturox@gmail.com>
2025-04-12 23:18:36 +0200

    initial set-up products

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { UserComponent } from './components/users/users.component';
 import { LoginComponent } from './components/login/login.component';
+import { ProductsFormComponent } from './components/products/products-form.component';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { LoginComponent } from './components/login/login.component';
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    UserComponent
+    UserComponent,
+    ProductsFormComponent
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule, routing],
   bootstrap: [AppComponent]
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { UserComponent } from './components/users/users.component';
+import { ProductsFormComponent } from './components/products/products-form.component';
+import { ProductsListComponent } from './components/products/products-list.component';
 
 const routes: Routes = [
   { path: 'users', component: UserComponent },
-  { path: 'auth/login', component: LoginComponent }
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'product-form', component: ProductsFormComponent },
+  { path: 'your-auctions', component: ProductsListComponent },
+  { path: 'your-winning-bids', component: ProductsListComponent }
 ];
 
 export const routing = RouterModule.forRoot(routes);
