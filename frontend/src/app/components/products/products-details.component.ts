881a8cbe70f51cae114af30797b3223bd349eb78
jae9104 <j.andres.2022@alumnos.urjc.es>
2025-04-19 22:55:25 +0200

    Added Street Map to Products

--- frontend/src/app/components/products/products-details.component.ts
+++ frontend/src/app/components/products/products-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { productsService } from '../../services/products.service';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,48 +10,56 @@ import { ActivatedRoute } from '@angular/router';
   //styleUrls: ['./products-details.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-    productId: number | undefined;
-    public userId: number | undefined;
-    product: any;
+  productId: number | undefined;
+  public userId: number | undefined;
+  product: any;
 
-    bidAmount: number | undefined;
-    bidMessage: string = '';
-    bidValid: boolean = false;
-  
-    constructor(private loginService: LoginService, private productsService: productsService, private route: ActivatedRoute) {}
-  
-    ngOnInit(): void {
-        this.route.paramMap.subscribe(params => {
-            const idParam = params.get('id');
-            this.productId = idParam ? +idParam : undefined;
-        
-            if (this.productId !== undefined) {
-              this.productsService.getProductById(this.productId).subscribe(
-                (data) => {
-                  console.log('Producto recibido:', data);
-                  this.product = data;
-                },
-                (error) => {
-                  console.error('Error al cargar el producto:', error);
-                }
-              );
-            } else {
-              console.error('Product ID no válido');
-            }
-          });
-        this.loginService.reqUser().subscribe((user) => {
-            this.userId = user.id;
-        });
-        
-    }
+  bidAmount: number | undefined;
+  bidMessage: string = '';
+  bidValid: boolean = false;
 
-    placeBid(): void {
-        if (this.bidAmount !== undefined && this.bidAmount > 3) {
-          this.bidMessage = 'Puja realizada correctamente';
-          this.bidValid = true;
-        } else {
-          this.bidMessage = 'Error al pujar. El valor debe ser mayor a 3';
-          this.bidValid = false;
-        }
+  safeMapUrl?: SafeResourceUrl;
+
+  constructor(private loginService: LoginService, private productsService: productsService, private route: ActivatedRoute, private sanitizer: DomSanitizer) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const idParam = params.get('id');
+      this.productId = idParam ? +idParam : undefined;
+
+      if (this.productId !== undefined) {
+        this.productsService.getProductById(this.productId).subscribe(
+          (data) => {
+            console.log('Producto recibido:', data);
+            this.product = data;
+            this.generateSafeMapUrl(this.product.seller.zipCode);
+          },
+          (error) => {
+            console.error('Error al cargar el producto:', error);
+          }
+        );
+      } else {
+        console.error('Product ID no válido');
       }
+    });
+    this.loginService.reqUser().subscribe((user) => {
+      this.userId = user.id;
+    });
+
   }
+
+  placeBid(): void {
+    if (this.bidAmount !== undefined && this.bidAmount > 3) {
+      this.bidMessage = 'Puja realizada correctamente';
+      this.bidValid = true;
+    } else {
+      this.bidMessage = 'Error al pujar. El valor debe ser mayor a 3';
+      this.bidValid = false;
+    }
+  }
+
+  private generateSafeMapUrl(zipCode: string): void {
+    const rawUrl = `https://www.google.com/maps?q=${encodeURIComponent(zipCode + ', España')}&output=embed`;
+    this.safeMapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(rawUrl);
+  }
+}
